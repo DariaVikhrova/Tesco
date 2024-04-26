6d874047b092ead6d559acaced97b7f6c114e5fa
DariaVikhrova <dashavihrova@gmail.com>
2024-04-26 12:48:51 +0200

    unnecessary import is removed

--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -5,7 +5,6 @@ import driver.DriverInitializer;
 import driver.Settings;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
-import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
